7ff59e6de1f38991da72f846573d4e015200d160
IT403JDY <49646133+IT403JDY@users.noreply.github.com>
2019-05-28 18:21:08 +0900

    Update GSPB
    66line !require => require
    fee is 10%
    event change

--- GSPB.sol
+++ GSPB.sol
@@ -1,4 +1,5 @@
 // Klaytn IDE uses solidity 0.4.24 version.
+//얼마씩 처 집어넣었는지 랑 점수를 넣지 못한다는 점이 문제네
 //클레이를 받는 부분
 pragma solidity ^0.4.24;
 
@@ -7,6 +8,7 @@ contract GSPB {
     struct student{
         string name;
         uint point;
+        uint[] attentStudies;
     }
     
     struct groupStudy{
@@ -15,6 +17,7 @@ contract GSPB {
         address[] participants;
         bool live; //private
         uint point;
+        uint limit;
         mapping(address=> uint) payment;
         //price?
     }
@@ -25,11 +28,10 @@ contract GSPB {
     mapping(address => student) public students;
     groupStudy[] public groupStudyList;//mapping을 사용하는것이 더 좋을듯하다.
 
-    event joinPlatformEvent(string _name);
-    event addGroupStudyEvnet(string _name, address[] _participants, uint _point);
-    event closeStudyEvent(uint _index, uint[] ranking);
+    event joinPlatformEvent(string _name, bool isJoin);
+    event addGroupStudyEvnet(string _name, address[] _participants, uint _point, uint groupStudyNum);
+    event closeStudyEvent(uint _index, uint[] ranking, uint klay);
     event depostieEvent(uint _index);
-
 
     constructor() public{
         owner = msg.sender;
@@ -37,14 +39,18 @@ contract GSPB {
     //가입
     function joinPlatform(string _name) public{
         require(!isJoin[msg.sender]);
-        students[msg.sender] = student(_name, 0);
+        uint[] memory x;
+        students[msg.sender] = student(_name, 0, x);
         isJoin[msg.sender] = true;
-        emit joinPlatformEvent(_name);
+        emit joinPlatformEvent(_name, isJoin[msg.sender]);
     }
     //스터디 생성
-    function addGroupStudy(string _name, address[] _participants, uint _point)public{
-        groupStudyList.push(groupStudy(_name, msg.sender, _participants, true, _point));
-        emit addGroupStudyEvnet(_name, _participants, _point);
+    function addGroupStudy(string _name, address[] _participants, uint _point, uint _limit)public {
+        groupStudyList.push(groupStudy(_name, msg.sender, _participants, true, _point, _limit));
+        for(uint i = 0; i < _participants.length; i++){
+            students[_participants[i]].attentStudies.push(groupStudyList.length-1);
+        }
+        emit addGroupStudyEvnet(_name, _participants, _point, groupStudyList.length-1);
     }
     //현재 컨트랙에 포함된 모든 KLAY
     function getBalance() public view returns (uint){
@@ -53,22 +59,29 @@ contract GSPB {
     //개인 마다 그룹스터디에 doposite한 KLAY를 저장
     function depostie(uint _index) public payable{
         groupStudyList[_index].payment[msg.sender] = msg.value;
+        //스터디에 해당하는 사람만 보낼 수 있도록 하는 함수는 node.js에서 구현한느것이 좋다.
+        //public로 해놔서 그냥 불서 볼 수 있다.
     }
     function closeStudy(uint _index, uint[] ranking) public returns (bool){
-        require(!groupStudyList[_index].live);
-        uint fee = 50000000000000000; //0.05 클레이
-        //ranking이 순위대로 왔다고 생가가하고 분배를 한다. [2,3,1,0]
-        uint tmp = groupStudyList[_index].point;
+        require(groupStudyList[_index].live);
         require(msg.sender==groupStudyList[_index].studyOwner);
+        uint returnKlay = 0;
+        uint tmp = groupStudyList[_index].point;
         for(uint i = 0; i < ranking.length; i++){
             address target = groupStudyList[_index].participants[ranking[i]];
+            uint fee = (groupStudyList[_index].payment[target])/10; //fee is 10%
             if (i == ranking.length-1){
                 students[target].point = tmp;
             }
             tmp = tmp/2;
             students[target].point = tmp;
             target.transfer(groupStudyList[_index].payment[target]-fee);
+            returnKlay += groupStudyList[_index].payment[target]-fee;
         }
+        groupStudyList[_index].live = false;
+        //클레이 재분배 0.9 만큼 일단 보낸다.
+        emit closeStudyEvent( _index, ranking, returnKlay);
+        
         return true;
         //평가를 web에서 모두 받고 관리자가 마무리를 누르면 오게된다.
     }
